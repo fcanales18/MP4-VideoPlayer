da1241d8c0a8054feb60eb3368dab6e418599ab9
Franco Canales <39963375+fcanales18@users.noreply.github.com>
2021-02-13 20:07:16 -0500

    Add files via upload

---
+++ MediaPlayer/src/mediaplayer/MediaPlayer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayer;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author fcana
+ */
+public class MediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Media Player");
+        
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+              if(doubleClicked.getClickCount() == 2) {
+                  stage.setFullScreen(true);
+              }
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
